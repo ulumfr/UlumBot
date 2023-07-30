f2048fac0807e7ba2913b02b5e58c3a93512727e
Bahrul 'Ulum Fadhlur Rohman <bahrululumfr@gmail.com>
2023-07-30 14:15:24 +0700

    update translate

--- src/commands/general/translate.js
+++ src/commands/general/translate.js
@@ -3,119 +3,39 @@ const translate = require('@iamtraction/google-translate');
 
 const languageMap = {
     "auto": "Automatic",
-    "af": "Afrikaans",
     "sq": "Albanian",
-    "am": "Amharic",
     "ar": "Arabic",
-    "hy": "Armenian",
-    "az": "Azerbaijani",
-    "eu": "Basque",
-    "be": "Belarusian",
-    "bn": "Bengali",
-    "bs": "Bosnian",
-    "bg": "Bulgarian",
-    "ca": "Catalan",
-    "ceb": "Cebuano",
-    "ny": "Chichewa",
-    "zh-cn": "Chinese Simplified",
     "zh-tw": "Chinese Traditional",
-    "co": "Corsican",
-    "hr": "Croatian",
-    "cs": "Czech",
-    "da": "Danish",
-    "nl": "Dutch",
     "en": "English",
-    "eo": "Esperanto",
-    "et": "Estonian",
     "tl": "Filipino",
-    "fi": "Finnish",
-    "fr": "French",
-    "fy": "Frisian",
-    "gl": "Galician",
-    "ka": "Georgian",
     "de": "German",
-    "el": "Greek",
-    "gu": "Gujarati",
-    "ht": "Haitian Creole",
-    "ha": "Hausa",
-    "haw": "Hawaiian",
-    "iw": "Hebrew",
     "hi": "Hindi",
-    "hmn": "Hmong",
-    "hu": "Hungarian",
-    "is": "Icelandic",
-    "ig": "Igbo",
     "id": "Indonesian",
-    "ga": "Irish",
     "it": "Italian",
     "ja": "Japanese",
     "jw": "Javanese",
     "kn": "Kannada",
-    "kk": "Kazakh",
-    "km": "Khmer",
     "ko": "Korean",
-    "ku": "Kurdish (Kurmanji)",
-    "ky": "Kyrgyz",
-    "lo": "Lao",
     "la": "Latin",
-    "lv": "Latvian",
-    "lt": "Lithuanian",
-    "lb": "Luxembourgish",
-    "mk": "Macedonian",
-    "mg": "Malagasy",
     "ms": "Malay",
-    "ml": "Malayalam",
-    "mt": "Maltese",
-    "mi": "Maori",
-    "mr": "Marathi",
     "mn": "Mongolian",
     "my": "Myanmar (Burmese)",
-    "ne": "Nepali",
-    "no": "Norwegian",
-    "ps": "Pashto",
-    "fa": "Persian",
-    "pl": "Polish",
     "pt": "Portuguese",
     "pa": "Punjabi",
-    "ro": "Romanian",
-    "ru": "Russian",
-    "sm": "Samoan",
-    "gd": "Scots Gaelic",
-    "sr": "Serbian",
-    "st": "Sesotho",
-    "sn": "Shona",
-    "sd": "Sindhi",
-    "si": "Sinhala",
-    "sk": "Slovak",
-    "sl": "Slovenian",
-    "so": "Somali",
     "es": "Spanish",
     "su": "Sundanese",
-    "sw": "Swahili",
-    "sv": "Swedish",
-    "tg": "Tajik",
-    "ta": "Tamil",
-    "te": "Telugu",
     "th": "Thai",
     "tr": "Turkish",
-    "uk": "Ukrainian",
-    "ur": "Urdu",
-    "uz": "Uzbek",
     "vi": "Vietnamese",
-    "cy": "Welsh",
-    "xh": "Xhosa",
-    "yi": "Yiddish",
-    "yo": "Yoruba",
-    "zu": "Zulu"
 };
-  
-const languageMapFrom = Object.entries(languageMap).slice(0, 25).map(([name, value]) => ({ name, value }));
-const languageMapTo = Object.entries(languageMap).slice(0, 25).map(([name, value]) => ({ name, value }));
-  
+
+const languageMapFrom = Object.entries(languageMap).slice(0, 25).map(([value, name]) => ({ value, name }));
+const languageMapTo = Object.entries(languageMap).slice(0, 25).map(([value, name]) => ({ value, name }));
+const languageInfo = {...Object.fromEntries(Object.entries(languageMap).map(([value, name]) => [value, { name }]))};
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('translate')
-        .setDescription('Translate your message to a different language.')
+        .setDescription('Translate your message to a different language (Only 25 Language).')
         .addStringOption(option => option
             .setName('text')
             .setDescription('message input')
@@ -144,9 +64,10 @@ module.exports = {
 
         const embed = new EmbedBuilder()
             .setColor("#FFAEC9")
+            .setTitle("📝 Translate 📝")
             .setFields(
-                { name: 'Inputted Text', value: msg },
-                { name: 'Translated Text', value: translated.text }
+                { name: `**Inputted ${languageInfo[from].name}**`, value: msg },
+                { name: `**Translate to ${languageInfo[to].name}**`, value: translated.text }
             )
             .setFooter({ text: `Google Translate`})
             .setTimestamp()
