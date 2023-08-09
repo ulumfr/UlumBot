8e80a3dfa7cfc8e29b532ab5f99cb1f19f9aa5fd
Bahrul 'Ulum Fadhlur Rohman <bahrululumfr@gmail.com>
2023-08-09 19:39:02 +0700

    add meme

---
+++ src/commands/fun/meme.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const axios = require('axios');
+const https = require('https');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('meme')
+        .setDescription('Get random meme.')
+        .setDMPermission(true),
+
+    async execute(interaction, client) {
+        await interaction.deferReply({ fetchReply: true });
+
+        try {
+            const agent = new https.Agent({  
+                rejectUnauthorized: false
+            });
+            
+            const res = await axios.get(`https://reddit.com/r/memes/random/.json`,{
+                httpsAgent: agent,
+                timeout: 10000,
+            })
+
+            const data = res.data[0].data.children[0].data;
+            const memeEmbed = new EmbedBuilder()
+                .setTitle(`${data.title}`)
+                .setURL(`https://reddit.com${data.permalink}`)
+                .setImage(`${data.url}`)
+                .setDescription(`${data.author}\n${data.selftext}`)
+                .setColor("#FFAEC9")
+                .setTimestamp()
+                .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() });
+
+            await interaction.reply({ embeds: [memeEmbed] });
+        } catch (error) {
+            await interaction.editReply({ content: 'TRY AGAIN!' });
+            await interaction.deleteReply()
+            console.log(error)
+        }
+    },
+};
